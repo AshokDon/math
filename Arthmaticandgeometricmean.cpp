2fd8620dc64b82eefda534690482d9356388019c
Ashok <68994450+AshokDon@users.noreply.github.com>
2021-05-18 21:15:02 +0530

    Add files via upload

---
+++ Arthmaticandgeometricmean.cpp
@@ -0,0 +1,27 @@
+/*Arthmatic Progression 
+2,4,6,8,10....
+every number has a difference of 2 --> first number a=2 and difference d=2
+2nd term -->a+d
+3rd term-->a+2d......nth term -->a+(n-1)d
+average = sum/n --> sum=avg*n --> we can get avg by (first term + last term)/2
+sum=n/2*(a+a+(n-1)d) --> n/2*(2a+(n-1)d)
+
+Geometric Progression 
+2,4,8,16,32...
+first term r=2 second term a*r, a*r^2,...nth term a*r^n-1
+sum=a(1-r^n)/1-r 
+
+Mean and median 
+Mean:- the mean is found by adding up on all the data and divide them by the number of 
+data entires 
+Example:- the mean of 5,8,2 is (5+8+2)/3-->5
+Median:- the middle number : found by odering all data and picking out one in the
+middle (or there are two middle numbers take the mean of the two numbers)
+Example :- the median of 5,8,2 is 5 because order them 2,5,8
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+}
